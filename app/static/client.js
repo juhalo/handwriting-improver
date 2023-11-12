61b34f4693537b1224c81cdcb3b6c234ba634149
juhalo <107361221+juhalo@users.noreply.github.com>
2023-11-12 21:02:40 +0200

    Make send button send img to server

--- app/static/client.js
+++ app/static/client.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById('drawing-board');
 const clearBtn = document.querySelector('#clear');
 const sendBtn = document.querySelector('#send');
 const ctx = canvas.getContext('2d');
+const prediction = document.getElementById('predictions');
 
 let isDrawing = false;
 
@@ -39,12 +40,27 @@ clearBtn.addEventListener('click', (_) => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
 
-sendBtn.addEventListener('click', (_) => {
-  var link = document.createElement('a');
-  link.setAttribute('download', 'MintyPaper.png');
-  link.setAttribute(
-    'href',
-    canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream')
-  );
-  link.click();
+sendBtn.addEventListener('click', async (_) => {
+  sendBtn.disabled = true;
+  var canvasData = canvas.toDataURL('image/png');
+  const formData = new FormData();
+  formData.append('file', canvasData);
+  const response = await fetch('/predict/', {
+    method: 'POST',
+    body: formData,
+  });
+
+  const json_response = await response.json();
+  prediction.innerHTML = json_response.prediction;
+  sendBtn.disabled = false;
 });
+
+// sendBtn.addEventListener('click', (_) => {
+//   var link = document.createElement('a');
+//   link.setAttribute('download', 'MintyPaper.png');
+//   link.setAttribute(
+//     'href',
+//     canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream')
+//   );
+//   link.click();
+// });
